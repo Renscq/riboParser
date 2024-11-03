e7e444968b94a610b467d02d92951cce8368e20b
rensc <rensc0718@163.com>
2024-11-03 08:41:50 +0800

    v0.1.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name="RiboParser",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     
     author="Ren Shuchao",
