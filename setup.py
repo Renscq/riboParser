53d259af8a55b9b401fa8eaae3cbea59cd4e94e8
rensc <rensc0718@163.com>
2024-11-17 13:41:51 +0800

    v0.1.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="RiboParser",
-    version="0.1.6",
+    version="0.1.6.1",
     packages=find_packages(),
     
     author="Ren Shuchao",
